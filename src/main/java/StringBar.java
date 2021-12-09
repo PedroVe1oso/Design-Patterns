c55748ddd8f72750bd1d530c450ae99963dc37b8
Pedro Veloso <pedroveloso@gmail.com>
2021-12-09 12:25:56 +0000

    Added OrderStringRecipeTest class and the order method to the StringBar class

--- src/main/java/StringBar.java
+++ src/main/java/StringBar.java
@@ -3,4 +3,8 @@ public class StringBar extends Bar{
     public StringBar() {
         super(false);
     }
+
+    public void order(StringDrink drink, StringRecipe recipe) {
+        recipe.mix(drink);
+    }
 }
