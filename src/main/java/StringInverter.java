20144a921e5ce0a6e8024b6eaca5ad61f65d8c9b
Pedro Veloso <pedroveloso@gmail.com>
2021-12-09 09:27:29 +0000

    Added StringInverter and StringInverterTest classes

---
+++ src/main/java/StringInverter.java
@@ -0,0 +1,8 @@
+public class StringInverter implements StringTransformer{
+
+    @Override
+    public void execute(StringDrink drink) {
+        StringBuilder text = new StringBuilder(drink.getText());
+        drink.setText(text.reverse().toString());
+    }
+}
